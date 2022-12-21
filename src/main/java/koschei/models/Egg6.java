08f5c2ef11cf751425ba1f956a382496533f858b
xGeorgy23 <gera2001.11.04@gmail.com>
2022-12-21 12:27:50 +0300

    task-2.1.4 refactor

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -3,12 +3,10 @@ package koschei.models;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Egg6 {
 
     private final Needle7 needle7;
 
-    @Autowired
     public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
